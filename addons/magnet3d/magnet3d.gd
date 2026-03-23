f4b3459fed9c48fb4b4dd3aad472666805e19dff
Dilonk1234 <fouracornspebbles@outlook.com>
2026-03-23 19:07:04 +0000

    finished plugin

--- addons/magnet3d/magnet3d.gd
+++ addons/magnet3d/magnet3d.gd
@@ -1,6 +1,23 @@
+@icon("uid://d8ey0axl4yyh")
 extends Area3D
 class_name Magnet3D
 
 @export var target : Node3D
 @export var strength := 20.0
-@export var damping := 5.0
+@export var damping_strength := 5.0
+
+func _physics_process(delta: float) -> void:
+	if not target:
+		push_error(get_path(), " Needs a target. Magnet will do nothing.")
+		return
+	
+	for x: Node3D in get_overlapping_bodies():
+		if not x is RigidBody3D:
+			continue
+		
+		var direction := target.global_position - x.global_position
+		var distance := direction.length()
+		var damping : Vector3 = -x.linear_velocity * damping_strength
+		if distance > 0:
+			var force := direction * strength
+			x.apply_force(force + damping)
